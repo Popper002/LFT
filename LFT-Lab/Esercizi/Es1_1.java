7b62b714bf63bec4a2fa986a98fbe8560337cffd
Riccardo Oro <riccardo.oro@edu.unito.it>
2023-05-06 17:36:13 +0200

    dfa 1.1 funzionante

--- LFT-Lab/Esercizi/Es1_1.java
+++ LFT-Lab/Esercizi/Es1_1.java
@@ -1,46 +1,41 @@
+/*DFA FUNZIONANTE  */
+
 public class Es1_1 {
-    
-
-//DA RIFARE --> CAPIRE COME CONTROLLARE L'PENULTIMA CIFRA E LETTERE MAIUSCOLE 
-
-
-
-
+   
     public static boolean scan(String s) {
 
         int state = 0;
         int i = 0;
         while (state >= 0 && i < s.length()) {
-            final char ch = s.charAt(i++);
-            
-        switch(state){ 
-
-         case -1: 
+        final char ch = s.charAt(i++);
+        switch(state)
+        { 
+            case -1:
                 state =-1; 
-                break; 
-         case 0: 
-                if (ch =='0'| ch =='1')
-                state =1; 
-                break; 
-         case 1: 
-            if (ch =='0'| ch =='1')
-                state =2; 
-         case 2: 
-                if (ch=='1')
-                state =3; 
-                if(ch == '0')
-                state =-1; 
+                System.out.println("CHARACTER NOT RECOGNIZED IN POSITION \n"+i);
                 break;
-        case 3: 
-            if ( ch == '0')
-            state = -1; 
-            break ; 
-
-        }  
-    } 
-        return state ==3; 
-    } 
-
+            case 0: 
+                if( ch=='0'||ch=='1')
+                {
+                    state =1; 
+                    break;
+                }
+                state =-1;
+                break; 
+          
+            case 1: 
+            if( ch=='0'||ch=='1')
+            {
+                state =1; 
+                break;
+            }
+            state =-1;
+            break;  
+        
+      }
+     }
+        return state ==1; 
+    }
 
     public static void main(String[] args) {
         System.out.println(scan("010101")? "OK":"NOPE");
